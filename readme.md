57e80f22a9ba967ce7591372ecab3d034f69162c
sharipovr <sharipov.rustem@gmail.com>
2026-09-06 22:11:42 -0700

    Add primitive tcp server and tcp client.

---
+++ readme.md
@@ -0,0 +1,5 @@
+Very high-level plan/memo.
+
+1) Create a workspace with four multi-projects.
+2) Start the development with basic tcp server. Run it with: ``` cargo run -p tcpserver```
+3) Add even more primitive tcp client. Run it with similarly.
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap(); // initialize socket server
+    println!("Running on port 3000");
+
+    // Next server listens for incoming connections (on iterator)
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap(); // if connection happens it unwraps (not proper solution of course), so it became either TCP stream or program panics
+        println!("Connection established");
+    }
 }
