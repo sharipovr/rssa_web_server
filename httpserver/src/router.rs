3528700069a6ceefd8cac37acec048c33dec9658
sharipovr <sharipov.rustem@gmail.com>
2026-09-14 15:09:21 -0700

    feat(httpserver): Complete main function and server, router and handler modules

---
+++ httpserver/src/router.rs
@@ -0,0 +1,35 @@
+// Router module inspects the incoming HTTP requests and figures out which handler to use
+use super::handler::{Handler, PageNotFoundHandler, StaticPageHandler, WebServiceHnadler};
+use http::{httprequest, httprequest::HttpRequest, httpresponse::HttpResponse};
+use std::io::prelude::*;
+pub struct Router;
+impl Router {
+    pub fn route(req: HttpRequest, stream: &mut impl Write) -> () {
+        match req.method {
+            // If GET request
+            httprequest::Method::Get => match &req.resource {
+                httprequest::Resource::Path(s) => {
+                    // Parse the URI
+                    let route: Vec<&str> = s.split("/").collect();
+                    match route[1] {
+                        // if the route begins with /api, invoke web service
+                        "api" => {
+                            let resp: HttpResponse = WebServiceHnadler::handle(&req);
+                            let _ = resp.send_response(stream);
+                        }
+                        // Else invoke static page handler
+                        _ => {
+                            let resp: HttpResponse = StaticPageHandler::handle(&req);
+                            let _ = resp.send_response(stream);
+                        }
+                    }
+                }
+            },
+            // If method is not GET request, return 404 page
+            _ => {
+                let resp: HttpResponse = PageNotFoundHandler::handle(&req);
+                let _ = resp.send_response(stream);
+            }
+        }
+    }
+}
